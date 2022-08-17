7bfd3cd4c85ef766ae26dbb7cdc690a3113b497b
Aalaizha <l.bnjamin0309@gmail.com>
2022-08-17 00:15:26 -0400

    Create grid that can change size based on user input

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 
-let container = document.querySelector(".container")
+let container = document.querySelector(".container");
+let gridSize = 16;
 
 
+// createGrid() sets the grid size to be the size inputed by the user. It then calls the createGridSquares() function to populate the created grid
 
-for(let i = 0; i < 256; i++)
+function createGrid()
 {
-    let div = document.createElement("div");
-    div.classList.add("grid-item")
-    div.style.backgroundColor = "lightgray"
-    div.style.height = "100px";
-    div.style.width = "100px";
-    
-    container.append(div);
+    container.style.gridTemplateColumns = (`repeat(${gridSize}, 1fr`);
+    container.style.gridTemplateRows = (`repeat(${gridSize}, 1fr`);
+
+    createGridSquares();
 }
 
+// createGridSquares multiples the gridSize by itself to calculate the total amount of squares needed for the grid. Once its calculated, the divs are created and added to the DOM
+
+function createGridSquares()
+{
+    let totalSquares = gridSize * gridSize;
+    
+    for(let gridItems = 0; gridItems < totalSquares; gridItems++)
+    {
+        let div = document.createElement("div");
+        div.classList.add("grid-item")
+
+        container.append(div);
+    }
+}
+
+
+createGrid();
