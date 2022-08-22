716881032ffafed83e8a4877ac2cad69c5790ee6
Aalaizha <l.bnjamin0309@gmail.com>
2022-08-22 17:30:37 -0400

    Add ability to clear the grid when the clear button is clicked

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 
 let canvas = document.querySelector(".canvas");
-let colorSquare = document.querySelectorAll(".color");
-
-
 let gridSize = 10;
-let cellColor = "black";
 
 // createGrid() sets the grid size to be the size inputed by the user. It then calls the createGridSquares() function to populate the created grid
 
@@ -16,7 +12,12 @@ function createGrid()
     createGridSquares();
 }
 
+createGrid();
+
 // createGridSquares multiples the gridSize by itself to calculate the total amount of squares needed for the grid. Once its calculated, the divs are created and added to the DOM
+
+let cellColor = "black";
+
 
 function createGridSquares()
 {
@@ -25,7 +26,7 @@ function createGridSquares()
     for(let gridItems = 0; gridItems < totalSquares; gridItems++)
     {
         let div = document.createElement("div");
-        div.classList.add("grid-item")
+        div.classList.add("gridItem")
 
         canvas.append(div);
 
@@ -38,6 +39,10 @@ function createGridSquares()
 }
 
 
+//Gets all divs with the class of color. When a div is clicked it makes the cell color equal to the color of the div.
+
+let colorSquare = document.querySelectorAll(".color");
+
 function changeBrushColor() {
     colorSquare.forEach(color => {
         color.onclick = () => {
@@ -47,6 +52,21 @@ function changeBrushColor() {
 }
 
 
-
-createGrid();
 changeBrushColor();
+
+//Gets all gridItem and loop through them. For each gridItem that is found, its background color will become light gray when the claer button is clicked
+
+let myGridItems = document.querySelectorAll(".gridItem")
+
+let clearBtn = document.querySelector(".clear-btn")
+
+clearBtn.addEventListener("click", clearGrid)
+
+function clearGrid()
+{
+    myGridItems.forEach(gridItem => {
+        gridItem.style.backgroundColor = "lightGray"
+    })
+}
+
+
