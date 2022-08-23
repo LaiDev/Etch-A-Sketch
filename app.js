99ad310d5feb95fd5a53e3333cd915e015ea46c6
Aalaizha <l.bnjamin0309@gmail.com>
2022-08-23 11:31:00 -0400

    Add functionality: New grid can be created based on the value of the slider

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-
 let canvas = document.querySelector(".canvas");
-let gridSize = 10;
 
-// createGrid() sets the grid size to be the size inputed by the user. It then calls the createGridSquares() function to populate the created grid
 
-function createGrid()
+// createGrid() sets the grid size to be the size inputed by the user. It then calls the createGridSquares() function to populate the created grid. By default the gridSize is set to 10
+
+function createGrid(gridSize)
 {
     canvas.style.gridTemplateColumns = (`repeat(${gridSize}, 1fr`);
     canvas.style.gridTemplateRows = (`repeat(${gridSize}, 1fr`);
+    console.log(`Create Grid : ${gridSize}`)
 
-    createGridSquares();
+    createGridSquares(gridSize);
 }
 
-createGrid();
+createGrid(10);
 
 // createGridSquares multiples the gridSize by itself to calculate the total amount of squares needed for the grid. Once its calculated, the divs are created and added to the DOM
 
 let cellColor = "black";
 
 
-function createGridSquares()
+function createGridSquares(gridSize)
 {
+    console.log(`Create GridGridSquares : ${gridSize}`)
+
     let totalSquares = gridSize * gridSize;
     
     for(let gridItems = 0; gridItems < totalSquares; gridItems++)
@@ -37,6 +39,9 @@ function createGridSquares()
     }
 
 }
+
+
+
 
 
 //Gets all divs with the class of color. When a div is clicked it makes the cell color equal to the color of the div.
@@ -56,7 +61,7 @@ changeBrushColor();
 
 //Gets all gridItem and loop through them. For each gridItem that is found, its background color will become light gray when the claer button is clicked
 
-let myGridItems = document.querySelectorAll(".gridItem")
+
 
 let clearBtn = document.querySelector(".clear-btn")
 
@@ -64,9 +69,36 @@ clearBtn.addEventListener("click", clearGrid)
 
 function clearGrid()
 {
+    let myGridItems = document.querySelectorAll(".gridItem")
+
     myGridItems.forEach(gridItem => {
         gridItem.style.backgroundColor = "lightGray"
     })
 }
+
+
+//updateGrid is called when the user clicks and releases the slider. The function then gets the value from the slider, destroys the current grid, and creates a new grid with the slider value
+
+
+let slider = document.querySelector("#slider");
+
+slider.addEventListener("mouseup", updateGrid)
+
+function updateGrid()
+{
+    let newGridValue = slider.value;
+    
+    let currentGridItems = document.querySelectorAll(".gridItem");
+
+
+    for(gridItem of currentGridItems)
+    {
+        gridItem.remove();
+    }
+
+    createGrid(newGridValue);
+    
+}
+
 
 
